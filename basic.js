1904f90d8f92335d41f20de376fb7439ed9fdd71
Aliz Shrestha <98611497+thealizstha@users.noreply.github.com>
2023-11-12 23:20:56 +0545

    setname

---
+++ basic.js
@@ -0,0 +1,12 @@
+// setname property
+
+var person = {
+  name : "Anu",
+  age  : 20,
+  set setname(n)
+  {
+    this.name = n.toUpperCase();
+  }
+};
+person.setname = "Apu";
+console.log(person);
